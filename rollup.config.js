f54c10d9271b252d56a7a76b5972fd93230cecb2
Eirik Backer <eirik.backer@gmail.com>
2018-08-29 12:30:36 +0200

    Use rollup instead of webpack. Use jest instead of mocha (#10)
    * Use rollup instead of webpack. Use jest instead of mocha
    * Fix ES5
    * remove mocha
    * Updates after pr

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+const buble = require('rollup-plugin-buble')
+const serve = require('rollup-plugin-serve')
+const {uglify} = require('rollup-plugin-uglify')
+const {version} = require('./package.json')
+
+export default [{
+  input: 'lib/nativebridge.js',
+  output: {
+    format: 'cjs',
+    file: 'lib/nativebridge.cjs.js',
+    name: 'nativebridge'
+  },
+  plugins: [buble()]
+}, {
+  input: 'lib/nativebridge.js',
+  output: {
+    format: 'umd',
+    banner: `/*! @nrk/nativebridge v${version} - Copyright (c) 2015-${new Date().getFullYear()} NRK */`,
+    file: 'lib/nativebridge.min.js',
+    name: 'nativebridge',
+    sourcemap: true
+  },
+  plugins: [
+    buble(),
+    uglify({output: {comments: /^!/}}),
+    !process.env.ROLLUP_WATCH || serve({
+      contentBase: 'lib',
+      headers: {'Content-Security-Policy': `default-src 'self' https://*.nrk.no; style-src 'self' https://*.nrk.no 'unsafe-inline'`}
+    })
+  ]
+}]
